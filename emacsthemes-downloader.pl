2bb61989ad69042f104bc5c7fe35a0133fd9ccb2
Brian Roels <broels@salsify.com>
2018-11-09 08:56:30 -0500

    Uses ssh instead of https

--- emacsthemes-downloader.pl
+++ emacsthemes-downloader.pl
@@ -14,9 +14,9 @@ my $themes_dest = "$ENV{HOME}/.emacs.d/themes";
 
 # clone the emacs-themes-site repo
 if (-d $working) {
-	`cd $working && git pull`
+  `cd $working && git pull`
 } else {
-	`git clone $themes_source $working`;
+  `git clone $themes_source $working`;
 };
 
 # clone all theme repos
@@ -24,27 +24,26 @@ opendir(my $clone, "$working/recipes") or die "Cannot open: $!";
 my @recipes = readdir($clone);
 splice(@recipes, 0, 2); # get rid of '.' and '..'
 foreach (@recipes) {
-	my $raw_json;
-	{
-		local $/;
-		open(my $recipe, '<', "$working/recipes/$_") or die "Cannot open: $!";
-		$raw_json = <$recipe>;
-		close $recipe;
-	}
-	my $recipe_json = decode_json($raw_json);
-	my $recipe_source = $$recipe_json{remoteSrc};
-	$recipe_source =~ tr/\\//d;
-	my @url = split('/', $recipe_source);
-	splice(@url, 5);
-	$recipe_source = join('/', @url) . '.git';
-	my $recipe_path = $recipe_dest . '/' . $$recipe_json{name};
-	if (-d "$recipe_path") {
-		`cd "$recipe_path" && git pull`;
-	} else {
-		`git clone $recipe_source "$recipe_path"`;
-	}
+  my $raw_json;
+  {
+    local $/;
+    open(my $recipe, '<', "$working/recipes/$_") or die "Cannot open: $!";
+    $raw_json = <$recipe>;
+    close $recipe;
+  }
+  my $recipe_json = decode_json($raw_json);
+  my $recipe_source = $$recipe_json{remoteSrc};
+  $recipe_source =~ tr/\\//d;
+  my @url = split('/', $recipe_source);
+  my @repo = @url[3..4];
+  $recipe_source = 'git@github.com:' . join('/', @repo) . '.git';
+  my $recipe_path = $recipe_dest . '/' . $$recipe_json{name};
+  if (-d "$recipe_path") {
+    `cd "$recipe_path" && git pull`;
+  } else {
+    `git clone $recipe_source "$recipe_path"`;
+  }
 };
 
 # copy the '*-theme.el's to the themes directory
 `find $recipe_dest -name "*-theme.el" -type f -exec cp {} $themes_dest \\;`;
-
